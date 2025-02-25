f2f5a058dd810c2e19d4631834292c7e07d43c0c
Soutaro078 <kkrt235kw@gmail.com>
2025-02-25 20:24:40 +0900

    Station03まで完了

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,29 +1,29 @@
-import { Component } from "react";
+import { Component } from 'react'
 
 class ErrorBoundary extends Component {
   constructor(props) {
-    super(props);
-    this.state = { hasError: false };
+    super(props)
+    this.state = { hasError: false }
   }
 
   static getDerivedStateFromError(error) {
     // 次のレンダリングでフォールバック UI が表示されるように状態を更新します
-    return { hasError: true };
+    return { hasError: true }
   }
 
   componentDidCatch(error, errorInfo) {
     // エラーログを外部サービスに送信する
-    console.error("ErrorBoundary caught an error", error, errorInfo);
+    console.error('ErrorBoundary caught an error', error, errorInfo)
   }
 
   render() {
     if (this.state.hasError) {
       // フォールバック UI をレンダリングします
-      return <h1>Something went wrong.</h1>;
+      return <h1>Something went wrong.</h1>
     }
 
-    return this.props.children; 
+    return this.props.children
   }
 }
 
-export default ErrorBoundary;
+export default ErrorBoundary
